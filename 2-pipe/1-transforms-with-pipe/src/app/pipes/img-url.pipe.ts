a6c73690adbeeefd3f94c561f66a6722c963327f
Dima <sendmail.dima@gmail.com>
2020-10-06 20:11:41 +0300

    1

--- 2-pipe/1-transforms-with-pipe/src/app/pipes/img-url.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/img-url.pipe.ts
@@ -1,12 +1,25 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { IProductImage } from '../../../../../shared/mocks/2-pipes/product';
+import {Pipe, PipeTransform} from '@angular/core';
+import {IProductImage} from '../../../../../shared/mocks/2-pipes/product';
 
 @Pipe({
   name: 'imgUrl',
 })
 // @ts-ignore
 export class ImgUrlPipe implements PipeTransform {
+ // public ci: any;
+
   public transform(images: IProductImage[] | undefined): string {
-    return '';
+    const list: string[] = ['10', '20', '30'];
+    //const [cr1] = list;
+    //const [ci] = list;
+
+    // ci.forEach( (value) => {
+    //   console.log(value);
+    // });
+    const [cr] = images;
+    // console.log(cr);
+    console.log(typeof cr);
+    return cr.url;
+
   }
 }
